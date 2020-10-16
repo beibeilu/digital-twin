3bc8e0e0df6c88a6e0783922e132656f92a50a20
beibeilu <bl643@cornell.edu>
2020-10-15 21:41:17 -0400

    census api

---
+++ Resources/Source/Data.md
@@ -0,0 +1,5 @@
+2010 Census Blocks
+Resources/Source/2010 Census Blocks.geojson
+
+2010 Census Tracts
+Resources/Source/2010 Census Tracts.geojson
--- map.js
+++ map.js
@@ -6,45 +6,18 @@ mapboxgl.accessToken = 'pk.eyJ1IjoieGJsdXgiLCJhIjoiY2tmc2w0MXJiMG81ZDJ5bndvMGNra
 
 var bounds = [
     // Source: New York City Census Tracts for 2010 US Census
-    [-74.257159, 40.495992], // Southwest coordinates
-    [-73.699215, 40.915568] // Northeast coordinates
+    [-74.34438,40.47392], // Southwest coordinates
+    [-73.68091,40.95147] // Northeast coordinates
 ];
 
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/xblux/ckfpnq1200lnb19l2ggguyiqt',
+    style: 'mapbox://styles/mapbox/light-v10',
+    // style: 'mapbox://styles/xblux/ckfpnq1200lnb19l2ggguyiqt',
     center: [40.70578, -73.978187],
     zoom: 1,
     maxBounds: bounds // Sets bounds as max
 });
-
-
-let data = [
-    {
-        location: [-73.96191,40.80762],
-        content: 'I like to eat my lunch here'
-    },
-    {
-        location: [-73.95936,40.80610],
-        content: '15 years ago, you could see over the trees'
-    },
-    {
-        location: [-73.96204,40.80994],
-        content: 'This was once tennis courts'
-    },
-]
-
-data.forEach(function(d) {
-
-    let marker = new mapboxgl.Marker()
-    marker.setLngLat(d.location)
-    marker.addTo(map)
-
-    let popup = new mapboxgl.Popup()
-    popup.setHTML(d.content)
-    marker.setPopup(popup)
-
-})
 
 // Click to display lng, lat
 map.on('click', function(event) {
@@ -55,20 +28,82 @@ map.on('click', function(event) {
     console.log("clicked:", lng, lat)
 
     document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5) + '/n'
-
-    let marker = new mapboxgl.Marker()
-    marker.setLngLat([lng,lat])
-    marker.addTo(map)
-
-    let popup = new mapboxgl.Popup()
-    popup.setHTML('Hi' + lng.toFixed(5) + "," + lat.toFixed(5))
-    marker.setPopup(popup)
 })
 
-// map.on('load', function () {
-//     map.addSource('tract', {
-//         'type': 'geojson',
-//         'data':
-//             'Resouces/2010 Census Tracts.geojson'
-//     });
-// })
+map.on('load', function () {
+    map.addSource('tract', {
+        'type': 'geojson',
+        'data':
+            'Resources/Data/tract.geojson'
+    });
+
+    map.addLayer({
+        "id": "tract_fill",
+        "type": "fill",
+        "source": "tract",
+        "paint": {
+            "fill-color": "#888",
+            "fill-opacity": 0.4
+        },
+    });
+
+    map.addLayer({
+        "id": "tract_line",
+        "type": "line",
+        "source": "tract",
+        "paint": {
+            "line-color": "#000",
+            "line-width": 0.5
+        },
+    });
+
+})
+
+var layers = ['0-1', '1-2', '2-3', '3-4', '4-5'];
+var colors = ['#FFEDA0', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A'];
+for (var i = 0; i < layers.length; i++) {
+    var layer = layers[i];
+    var color = colors[i];
+    var item = document.createElement('div');
+    var key = document.createElement('span');
+    key.className = 'legend-key';
+    key.style.backgroundColor = color;
+    var value = document.createElement('span');
+    value.innerHTML = layer;
+    item.appendChild(key);
+    item.appendChild(value);
+    legend.appendChild(item);
+}
+
+// map.on('load', function() {
+//     // the rest of the code will go in here
+//     var layers = ['0-1', '1-2', '2-3', '3-4', '4-5'];
+//     var colors = ['#FFEDA0', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A'];
+//
+//     for (var i = 0; i < layers.length; i++) {
+//         var layer = layers[i];
+//         var color = colors[i];
+//         var item = document.createElement('div');
+//         var key = document.createElement('span');
+//         key.className = 'legend-key';
+//         key.style.backgroundColor = color;
+//
+//         var value = document.createElement('span');
+//         value.innerHTML = layer;
+//         item.appendChild(key);
+//         item.appendChild(value);
+//         legend.appendChild(item);
+//     }
+// });
+//
+map.on('mousemove', function(e) {
+    var tract = map.queryRenderedFeatures(e.point, {
+        layers: ['tract_fill']
+    });
+
+    if (tract.length > 0) {
+        document.getElementById('pd').innerHTML = '<h3><strong>' + tract[0].properties.ntaname + '</strong></h3><p>' + tract[0].properties.GeoId + '</p>';
+    } else {
+        document.getElementById('pd').innerHTML = '<p>Move around!</p>';
+    }
+});
