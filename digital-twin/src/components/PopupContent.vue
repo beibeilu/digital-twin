0d02a857f5136343c7ea940a0cbe8986bfc23fa6
thristram <thristram@gmail.com>
2020-10-23 00:29:54 -0400

    Add popup

---
+++ digital-twin/src/components/PopupContent.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<h3>{{ feature.title }}</h3>
+		<p>Coordinates: {{ feature.coordinates[0] }}, {{ feature.coordinates[1] }}</p>
+		<p>Area: {{ feature.area }}</p>
+	</div>
+</template>
+
+<script>
+	import Vue from 'vue'
+	export default Vue.extend({
+		props: {
+			feature: {
+				required: true,
+				type: Object,
+			},
+		},
+		methods: {
+			popupClicked() {
+				alert('Learn more clicked')
+			},
+		},
+	})
+</script>
